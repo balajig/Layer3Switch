d5c9eb418555ffe5bb808f8df22084c7a04929cf
Sasikanth V <sasikanth.v19@gmail.com>
2012-01-04 11:13:39 +0530

    [Layer3Switch] Fixed some cli warnings and added "ip default-gateway" command

--- src/route/route_cli.c
+++ src/route/route_cli.c
@@ -1,6 +1,6 @@
 #include "common_types.h"
 #include "cli.h"
-#include "ifmgmt.h"
+#include "route.h"
 #include "cparser.h"
 #include "cparser_tree.h"
 
@@ -9,4 +9,16 @@ cparser_result_t cparser_cmd_show_ip_route(cparser_context_t *context)
 	if (!cli_show_ip_route ())
                 return CPARSER_OK;
         return CPARSER_NOT_OK;
+}
+
+
+cparser_result_t cparser_cmd_config_ip_default_gateway_addr(cparser_context_t *context, uint32_t *addr_ptr)
+{
+	uint8_t addr[4];
+	uint32_2_ipstring (ntohl(*addr_ptr), &addr);
+
+	if (!route_add_gateway ("0.0.0.0", 0, addr))
+		return CPARSER_OK;
+	printf ("Couldn't find any vaild interface\n");
+	return CPARSER_NOT_OK;
 }
