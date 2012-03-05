98ed36adb8bbbc973885ddfae09234b3150e7134
Sasikanth V <sasikanth.v19@gmail.com>
2012-03-06 01:26:15 +0530

    Added Cross-compile support

--- src/build/make.h
+++ src/build/make.h
@@ -14,11 +14,11 @@ endif
 
 COMMON_INC= $(CODE_DIR)/inc  -I$(CODE_DIR)/lib -I$(CODE_DIR)/cli/inc -I$(CODE_DIR)/cli/src
 
-CC   = echo $(notdir $<);gcc
+CC   = echo $(notdir $<);$(CROSS_COMPILE)gcc
 CXXFLAGS = $(CXXINCS)  
 RM = rm -f
 DEBUG_FLAGS = -g -DDEBUG
-LD = ld
+LD = $(CROSS_COMPILE)ld
 LD_FLAGS = -r
 VERSION= 0.1
 CFLAGS = $(DEBUG_FLAGS) -Wall -Wsign-compare -Wpointer-arith -Wbad-function-cast -Wwrite-strings -Wmissing-prototypes -Wmissing-declarations -Wchar-subscripts -Wcast-qual -Wpadded
