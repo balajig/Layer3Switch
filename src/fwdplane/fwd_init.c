9b37cd5982876de60828d3aad7bd474541d935e8
Sasikantha babu <sasikanth.v19@gmail.com>
2012-03-22 18:35:26 +0530

    Software forwarding

---
+++ src/fwdplane/fwd_init.c
@@ -0,0 +1,26 @@
+/*
+ *  Author:
+ *  Sasikanth.V        <sasikanth@email.com>
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation; either version
+ *  2 of the License, or (at your option) any later version.
+ */
+
+int fplane_init ()
+{
+
+
+}
+
+
+int forward_packet ()
+{
+
+}
+
+int switch_packet ()
+{
+
+}
