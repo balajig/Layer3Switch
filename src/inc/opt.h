117d4fb973618649997bda967cab813ad0ac600f
Sasikanth V <sasikanth.v19@gmail.com>
2012-03-23 03:45:21 +0530

    Disable socket debugging

--- src/inc/opt.h
+++ src/inc/opt.h
@@ -1836,7 +1836,7 @@
  * SOCKETS_DEBUG: Enable debugging in sockets.c.
  */
 #ifndef SOCKETS_DEBUG
-#define SOCKETS_DEBUG                   LWIP_DBG_ON
+#define SOCKETS_DEBUG                   LWIP_DBG_OFF
 #endif
 
 /**
