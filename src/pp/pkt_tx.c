74c5a8447c49caecea3f00201e636eb95f0204c0
Sasikantha babu <sasikanth.v19@gmail.com>
2012-05-23 01:47:58 +0530

    Base framework for PP

---
+++ src/pp/pkt_tx.c
@@ -0,0 +1,33 @@
+/*
+ *  Author:
+ *  Sasikanth.V        <sasikanth@email.com>
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation; either version
+ *  2 of the License, or (at your option) any later version.
+ */
+
+
+uint8_t  *tx_chain;
+
+int pkt_init_tx_chain (void)
+{
+
+
+}
+
+int fast_proto_send ()
+{
+	/*
+ *      Send packet out immediatly
+ * 	*/
+}
+
+int slow_proto_send ()
+{
+	/*
+ *      Can be delayed for 50ms to 250ms eg: STP
+ * 	*/
+
+}
