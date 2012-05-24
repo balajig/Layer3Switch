7ddd14dce4dc5f9fca79c74a3bdac6ba4ee66c17
Sasikantha babu <sasikanth.v19@gmail.com>
2012-05-25 03:52:16 +0530

    dump_stack util to dump the stack

--- src/lib/util.c
+++ src/lib/util.c
@@ -151,3 +151,27 @@ char*  hex2bin(char *dst, const char *str, int count)
     errno = (*str ? ERANGE : 0);
     return dst;
 }
+
+void dump_stack (void)
+{
+	int j, nptrs;
+#define SIZE 100
+	void *buffer[100];
+	char **strings;
+
+	nptrs = backtrace(buffer, SIZE);
+	printf("backtrace() returned %d addresses\n", nptrs);
+
+	strings = backtrace_symbols(buffer, nptrs);
+	if (strings == NULL) {
+		perror("backtrace_symbols");
+		return;
+	}
+
+	for (j = 0; j < nptrs; j++)
+		printf("%s\n", strings[j]);
+
+	free(strings);
+}
+
+
