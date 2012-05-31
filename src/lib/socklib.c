418d1ffea1b6380902d51c1fc6602f13549b9790
Sasikantha babu <sasikanth.v19@gmail.com>
2012-05-31 20:54:57 +0530

    Added DS for Sock lib

--- src/lib/socklib.c
+++ src/lib/socklib.c
@@ -12,6 +12,92 @@
 #include "sockets.h"
 #include "socks.h"
 
+#ifdef UNDER_DEV
+
+#define MAX_SOCK_LAYER 12
+
+struct sock_layer {
+	sync_lock_t  lock;
+	int          sock_id;
+	int          familiy;
+	int 	     flags;
+	uint32_t     addr;
+	int          index;
+}SOCK_T;
+
+static SOCK_T *sock_layer[MAX_SOCK_LAYER];
+static sync_lock_t  lock;
+
+struct sock_client {
+	int       familiy;
+	int       sock
+	uint32_t  addr;
+	int       flags;
+	union proto {    
+		struct sock_tcp {
+			int port;
+			int listen;
+		}tcp;
+		struct sock_udp {
+			int  port;
+		}udp;
+	}p;
+	int       protocol;
+};
+
+enum {
+	SOCK_CLIENT_BIND  = 0x1,
+	SOCK_CLIENT_CONNECT = 0x2,
+	SOCK_CLIENT_NEW_THREAD = 0x4,
+	SOCK_CLIENT_BLOCKING = 0x8,
+};
+
+int sock_client_init (struct sock_client *client)
+{
+	if (!client)
+		return 0;
+	client->familiy    = -1;
+	client->flags      =  0;
+	client->addr       =  0;
+}
+
+int sock_client_register (struct sock_client *new)
+{
+	sync_lock (&lock);
+
+
+	sync_unlock (&lock);
+}
+
+/*Caller must hold the main sock layer lock*/
+SOCK_T * alloc_new_sock_layer  (void)
+{
+	int i = MAX_SOCK_LAYER;
+	
+	while (i >= 0) {
+		if (!sock_layer[i]) {
+			SOCK_T *p = tm_calloc (1, sizeof (SOCK_T));
+			if (!p) {
+				printf ("Error : Out of memory");
+				return NULL;
+			}
+			sock_layer[i] = p;
+			return p;
+		}
+	}
+	return NULL;
+}
+
+void release_sock_layer (SOCK_T *p)
+{
+	if (p) {
+		sock_layer[p->index] = NULL;
+		tm_free (p, sizeof (*p));
+	}
+}
+
+#endif
+
 int udp_v4_create (struct sockaddr_in *addr)
 {
 	int sock = -1;
